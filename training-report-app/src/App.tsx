69769bcbc7159a1c0e807a047f3af0dc30d536d6
Mars <mars@liuqianMB.local>
2025-04-13 19:56:44 +0800

    修正 APP.tsx 的 Router問題

--- training-report-app/src/App.tsx
+++ training-report-app/src/App.tsx
@@ -1,15 +1,15 @@
 // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import DailyReportForm from './pages/DailyReportForm';
 import ProgressOverview from './pages/ProgressOverview';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<ProgressOverview />} />
-        <Route path="/report" element={<DailyReportForm />} />
-        <Route path="/progress" element={<ProgressOverview />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<ProgressOverview />} />
+      <Route path="/report" element={<DailyReportForm />} />
+      <Route path="/progress" element={<ProgressOverview />} />
+    </Routes>
   );
 }
 
